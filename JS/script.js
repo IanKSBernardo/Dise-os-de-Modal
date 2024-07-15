253ad6f596379f68c798ed3b7b8b05c6f09237e8
Ian Benrardo <145799407+IanKSBernardo@users.noreply.github.com>
2024-07-14 22:53:28 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+// 1° MODAL
+const modal1 = document.querySelector(".modal-1");
+const openButton1 = document.querySelector(".openBTN-1");
+const closeButton1 = document.querySelector(".closeBTN-1");
+
+openButton1.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal1.showModal();
+});
+
+closeButton1.addEventListener("click", (e)=>{
+    e.preventDefault
+    modal1.close();
+});
+
+
+// 2° MODAL
+const modal2 = document.querySelector(".modal-2");
+const openButton2 = document.querySelector(".openBTN-2");
+const closeButton2 = document.querySelector(".closeBTN-2");
+
+openButton2.addEventListener("click", (e)=>{
+    e.preventDefault();
+    modal2.showModal();
+});
+
+closeButton2.addEventListener("click", (e)=>{
+    e.preventDefault
+    modal2.close();
+});
+
+// BODY
+
